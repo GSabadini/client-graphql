faa1db77147558114466533bddd072d42bef7453
Gabriel Sabadini Facina <gabriel.facina@uds.com.br>
2018-11-27 08:19:44 -0200

    First commit

---
+++ src/components/Users.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <div>
+    <pre>{{ users }}</pre>
+    <pre>{{ teste }}</pre>
+    <pre>{{ groups }}</pre>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag';
+export default {
+  apollo: {
+    users: gql`query {
+      users(id: 3) {
+        data {
+          id
+          name
+        }
+        per_page
+        total
+        from
+        to
+        current_page
+      }
+    }`,
+    groups: gql`query {
+      groups {
+        id
+      }
+    }`
+  },
+  computed: {
+    teste() {
+      console.log(this.$apolloData.data);
+    },
+  },
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Users from '@/components/Users.vue';
 
 Vue.use(Router);
 
@@ -10,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'apollo-example',
+      component: Users,
     },
     {
       path: '/about',
