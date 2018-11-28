fc2d3d2b591198855e064b4271f55bd2949ff1fe
Gabriel Sabadini Facina <gabriel.facina@uds.com.br>
2018-11-28 19:27:43 -0200

    Refatorado código

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,81 +1,106 @@
 <template lang="html">
   <div>
-    <p v-if="loading">Carregando...</p>
-    <ul>
-      <li v-for="(user,key) in users.data" :key="key">
-        {{ user.id }} -
-        {{ user.name }} |
-        {{ user.group.descricao }}
-      </li>
-    </ul>
-    <button @click="carregar = !carregar">Carregar informações</button>
-    <button @click="id++">Add ID</button>
+    <div v-if="$apollo.loading">
+      <div class="loader"></div>
+      <p>Buscando dados...</p>
+    </div>
+    <div v-if="user.id">
+      <hr>
+      <h2>USUÁRIO</h2>
+      <p>ID: {{ user.id }}</p>
+      <p>NOME: {{ user.name }}</p>
+      <p>GRUPO: {{ user.group.descricao }}</p>
+    </div>
+    <div v-if="user.posts.length">
+      <h4>POSTS</h4>
+      <ul>
+        <li v-for="post in user.posts">
+          {{ post.title }} - {{ post.body}}
+        </li>
+      </ul>
+    </div>
+    <div v-if="groups.length">
+      <hr>
+      <h2>GRUPOS</h2>
+      <ul>
+        <li v-for="(grupos, key) in groups" :key="key">{{ grupos.descricao }}</li>
+      </ul>
+    </div>
+    <button @click="makeRequest()">Carregar informações</button>
+    <button @click="incrementId()" v-if="!lazyLoading">Buscar proximo usuário</button>
   </div>
 </template>
 
 <script>
 import gql from 'graphql-tag';
 const userQuery = gql`query($id: Int) {
-  users(id: $id) {
-    data {
-      id
-      name
-      group {
-        descricao
-      }
+  user(id: $id) {
+    id
+    name
+    group {
+      descricao
     }
-    per_page
-    total
-    from
-    to
-    current_page
+    posts {
+      title
+      body
+    }
+  }
+  groups {
+    descricao
   }
 }`
 
 export default {
   data: () => ({
     id: 1,
-    users: {
-      data: [],
+    user: {
+      id: null,
+      name: null,
+      group: {},
+      posts: [],
     },
-    loading: false,
-    carregar: true,
+    groups: [],
+    lazyLoading: true,
   }),
   apollo: {
-    users: {
+    data: {
       query: userQuery,
-      loadingKey: 'loading',
       variables() {
         return { id: this.id }
       },
       skip() {
-        return this.carregar
+        return this.lazyLoading
+      },
+      update({ user, groups }) {
+        this.user = user
+        this.groups = groups
       },
     },
-    // groups: gql`query {
-    //   groups {
-    //     id
-    //   }
-    // }`
   },
   methods: {
-    getData() {
-      console.log(this.$apollo);
-      console.log(this.$apollo.query(
-        {query: userQuery}
-      ));
-      this.$apollo.query(
-        {query: userQuery, loadingKey: this.loading}
-      ).then(response => console.log(response))
-      // this.$apollo.queries.users.destroy();
-      // this.$apollo.queries.users.skip = false;
-      // this.$apollo.queries.users.refetch();
+    makeRequest() {
+      this.lazyLoading = false;
     },
-  },
-  computed: {
-    teste() {
-      console.log(this.$apolloData.data);
-    },
+    incrementId() {
+      this.id++;
+    }
   },
 };
 </script>
+
+<style lang="css">
+.loader {
+  margin: 0 auto;
+  border: 16px solid #f3f3f3;
+  border-top: 16px solid #3498db;
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
